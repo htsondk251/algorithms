46c9b4367871b355dd48b83f05764c09e6d10f83
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-08-04 17:32:54 +0800

    heap: top K frequent numbers

---
+++ src/main/java/com/example/algorithms/heap/topk/TopKFrequentNumbersFinder.java
@@ -0,0 +1,29 @@
+package com.example.algorithms.heap.topk;
+
+import java.util.*;
+
+public class TopKFrequentNumbersFinder {
+    public List<Integer> findTopKFrequentNumbers(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.merge(num, 1, Integer::sum);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> topFreq = new PriorityQueue<>(k, Map.Entry.comparingByValue());
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            topFreq.add(entry);
+            if (topFreq.size() > k) {
+                topFreq.poll();
+            }
+        }
+
+        List<Integer> topNumbers = new ArrayList<>(k);
+        for (Map.Entry<Integer, Integer> entry : topFreq) {
+            topNumbers.add(entry.getKey());
+        }
+        return topNumbers;
+    }
+
+
+}
