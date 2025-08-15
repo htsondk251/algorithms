601f3fee6faa71a25b301923aaf8d2ff651388f6
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-08-15 13:43:37 +0800

    tree: add base TreeNode

---
+++ src/main/java/com/example/datastructures/levelordertraversal/TreeNode.java
@@ -0,0 +1,17 @@
+package com.example.algorithms.levelordertraversal;
+
+/**
+ * @version 1.0
+ * @description:
+ * @author: sonhoangthanh
+ * @date: 11/08/2025 12:34
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
