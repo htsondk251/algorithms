ea992e7d8cfba1f63e527b489a951f97ddcccdea
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-01 23:14:20 +0700

    binary search: refactor - 153 Find Minimum In Rotate Sorted Array

--- src/main/java/com/example/test/medium/_153_FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/example/test/medium/_153_FindMinimumInRotatedSortedArray.java
@@ -9,28 +9,28 @@ package com.example.test.medium;
 
 public class _153_FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
-        return findMin(nums, 0, nums.length - 1);
-    }
+        int left = 0;
+        int right = nums.length - 1;
 
-    private int findMin(int[] nums, int left, int right) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
 
-        if (left == right) return nums[left];
-        if (left == right - 1) return Math.min(nums[left], nums[right]);
-        int mid = (left + right) /2;
-        if (nums[left] > nums[mid]) {
-            return findMin(nums, left, mid);
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+
+            } else {
+                right = mid;
+
+            }
         }
-
-        if (nums[mid] > nums[right]) {
-            return findMin(nums, mid, right);
-        }
-
-        return Math.min(nums[left], nums[mid]);
+        return nums[left];
     }
 
     public static void main(String[] args) {
         _153_FindMinimumInRotatedSortedArray i = new _153_FindMinimumInRotatedSortedArray();
-//        System.out.println(i.findMin(new int[]{0,1,2,4,5,6,7}));
+        System.out.println(i.findMin(new int[]{0,1,2,4,5,6,7}));
         System.out.println(i.findMin(new int[]{3,4,5,1,2}));
+        System.out.println(i.findMin(new int[]{4,5,6,7,0,1,2}));
     }
 }
