5bef24fa728aa396276a680aaa2528f660301e98
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-03 13:29:26 +0800

    matrix: refactor 238 Product Of Array Except Self

--- src/main/java/com/example/test/medium/_238_ProductOfArrayExceptSelf.java
+++ src/main/java/com/example/test/medium/_238_ProductOfArrayExceptSelf.java
@@ -9,22 +9,17 @@ package com.example.test.medium;
 public class _238_ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
         int[] result = new int[n];
 
-        left[0] = 1;
-        right[n-1] = 1;
+        result[0] = 1;
         for (int i = 1; i <= n-1; i++) {
-            left[i] = left[i-1] * nums[i - 1];
+            result[i] = result[i-1] * nums[i - 1];
         }
 
+        int suffix = 1;
         for (int i = n-2 ; i >= 0; i--) {
-            right[i] = right[i+1] * nums[i+1];
-        }
-
-        for (int i = 0; i < n; i++) {
-            result[i] = left[i] * right[i];
+            suffix *= nums[i+1];
+            result[i] *= suffix;
         }
         return result;
     }
