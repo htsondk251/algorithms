9f35a38057d21fdf4bd759be914f6cf5769ac02a
son.hoangthanh <son.hoangthanh@vti.com.vn>
2025-09-08 06:52:24 +0700

    tree: refactor - 105 Construct BTree from preorder and inorder

--- src/main/java/com/example/test/medium/_105_ConstructBT.java
+++ src/main/java/com/example/test/medium/_105_ConstructBT.java
@@ -2,36 +2,36 @@ package com.example.test.medium;
 
 import com.example.algorithms.levelordertraversal.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _105_ConstructBT {
+    private Map<Integer, Integer> inorderIndexMap;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        //preorder[0] = root
-        //rootIndex = inorder.get(root);
-        //inorder[0..rootIndex] = left
-        //inorder[rootIndex+1..n-1] = right;
-        return buildTreeRecursive(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIndexMap.put(inorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1, 0, inorder.length - 1);
     }
 
-    private TreeNode buildTreeRecursive(int[] preorder, int preStart, int preEnd,
-                                        int[] inorder, int inStart, int inEnd) {
-        if (preEnd < preStart ) return null;
+    private TreeNode build(int[] preorder, int preStart, int preEnd,
+                           int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd) return null;
+
         int rootVal = preorder[preStart];
         TreeNode root = new TreeNode(rootVal);
-        int rootIndex = findRootIndex(inorder, inStart, inEnd, rootVal);
-        int leftLen = rootIndex - inStart;
-        //left preorder, prestart+1, prestart+1+leftLen-1, inorder, inStart, inStart+leftLen-1
-        root.left = buildTreeRecursive(preorder, preStart+1, preStart+leftLen,
-                inorder, inStart, rootIndex - 1);
-        //right preorder,
-        root.right = buildTreeRecursive(preorder, preStart+leftLen+1, preEnd,
-                inorder, rootIndex+1, inEnd);
-        return root;
-    }
 
-    private int findRootIndex(int[] inorder, int inStart, int inEnd, int rootVal) {
-        for (int i = inStart; i <= inEnd; i++) {
-            if (rootVal == inorder[i]) return i;
-        }
-        return -1;
+        int rootIndex = inorderIndexMap.get(rootVal);
+        int leftSize = rootIndex - inStart;
+
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inStart, rootIndex - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                rootIndex + 1, inEnd);
+
+        return root;
     }
 
     public static void main(String[] args) {
