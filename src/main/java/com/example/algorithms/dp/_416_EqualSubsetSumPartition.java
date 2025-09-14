97fe28b629008fcb44e5a172d7d644a1c4e0062f
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-14 15:07:20 +0700

    dp: 416 Equal Subset Sum Partition

--- src/main/java/com/example/algorithms/dp/_416_EqualSubsetSumPartition.java
+++ src/main/java/com/example/algorithms/dp/_416_EqualSubsetSumPartition.java
@@ -16,19 +16,27 @@ public class _416_EqualSubsetSumPartition {
             sum += num[i];  //refactor: over
         }
         if (sum % 2 == 1) return false;
-        return canPartition(num, 0, sum/2);
+
+        Boolean[][] dp = new Boolean[num.length][sum/2+1];
+        return canPartition(num, 0, sum/2, dp);
     }
 
-    private boolean canPartition(int[] num, int startIndex, int target) {
+    private boolean canPartition(int[] num, int startIndex, int target, Boolean[][] dp) {
         if (target == 0) return true;
+        if (target < 0 || startIndex == num.length) return false;
 
-        if ( startIndex == num.length) return false;
+        if (dp[startIndex][target] != null) return dp[startIndex][target];
+        dp[startIndex][target] =canPartition(num, startIndex+1, target, dp) || canPartition(num, startIndex + 1, target - num[startIndex], dp);
 
-        return canPartition(num, startIndex+1, target) || canPartition(num, startIndex + 1, target - num[startIndex]);
+
+        return dp[startIndex][target];
     }
 
     public static void main(String[] args) {
         _416_EqualSubsetSumPartition i = new _416_EqualSubsetSumPartition();
-        System.out.println(i.canPartition(new int[]{2,3,4,6}));
+//        System.out.println(i.canPartition(new int[]{2,3,4,6}));
+//        System.out.println(i.canPartition(new int[]{1,2,3,4}));
+//        System.out.println(i.canPartition(new int[]{1,1,3,4,7}));
+        System.out.println(i.canPartition(new int[]{2,2,3,5}));
     }
 }
