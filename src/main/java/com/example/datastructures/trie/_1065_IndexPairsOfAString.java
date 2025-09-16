f384716c2e3af4a862e5c0b2dcef36785c904174
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-16 18:20:55 +0800

    Trie: 1065 Index Pairs of a String - refactor

--- src/main/java/com/example/datastructures/trie/_1065_IndexPairsOfAString.java
+++ src/main/java/com/example/datastructures/trie/_1065_IndexPairsOfAString.java
@@ -11,22 +11,33 @@ import java.util.*;
 public class _1065_IndexPairsOfAString {
     public List<List<Integer>> indexPairs(String text, List<String> words) {
         List<List<Integer>> result = new ArrayList<>();
-
+        // ToDo: Write Your Code Here.
         Trie trie = new Trie();
         words.forEach(trie::insert);
-        for (int i = 0; i < text.length(); i++) {
+
+        int n = text.length();
+        for (int i = 0; i < n; i++) {
             TrieNode node = trie.root;
-            int currI = i;
-            int index = text.charAt(currI) - 'a';
-            while (node.children[index] != null) {
-                node = node.children[index];
-                if (node.isEnd) {
-                    result.add(Arrays.asList(i, currI));
+            int textIndex = i;
+
+            while (textIndex < n) {
+                int index = text.charAt(textIndex) - 'a';
+
+                if (node.children[index] == null) {
                     break;
                 }
-                index = text.charAt(++currI) - 'a';
+
+
+                node = node.children[index];
+
+                if (node.isEnd) {
+                    result.add(Arrays.asList(i, textIndex));
+                }
+
+                textIndex++;
             }
         }
+
         return result; // Return a list of lists containing index pairs
     }
 
@@ -35,8 +46,8 @@ public class _1065_IndexPairsOfAString {
 //        System.out.println(i.indexPairs("aaabbbccc", Arrays.asList("aa", "bb", "cc"))); //[[0,1], [1,2], [3,4], [4,
         // 5], [6,7], [7,8]
 
-//        System.out.println(i.indexPairs("bluebirdskyscraper", Arrays.asList("blue", "bird", "sky")));
-        System.out.println(i.indexPairs("programmingisfun", Arrays.asList("pro", "is", "fun", "gram")));
+        System.out.println(i.indexPairs("bluebirdskyscraper", Arrays.asList("blue", "bird", "sky")));
+//        System.out.println(i.indexPairs("programmingisfun", Arrays.asList("pro", "is", "fun", "gram")));
     }
 
 
