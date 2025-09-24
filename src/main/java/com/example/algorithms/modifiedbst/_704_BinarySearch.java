2465fbd3f574e1333ae7c9bdc1a84ab761ad8040
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-24 13:25:04 +0800

    bs: refactor - 704 Binary Search

--- src/main/java/com/example/algorithms/modifiedbst/_704_BinarySearch.java
+++ src/main/java/com/example/algorithms/modifiedbst/_704_BinarySearch.java
@@ -8,21 +8,27 @@ package com.example.algorithms.modifiedbst;
  */
 public class _704_BinarySearch {
     public int search(int[] nums, int target) {
-        return bst(nums, 0, nums.length - 1, target);
-    }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = right - (right - left) / 2;
+            if (nums[mid] == target) { return mid;}
 
-    private int bst(int[] nums, int left, int right, int target) {
-        if (left > right || left == right && nums[left] != target) {return -1;}
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) return mid;
-        if (nums[mid] < target) return bst(nums, mid+1, right, target);
-        else return bst(nums, left, mid, target);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         _704_BinarySearch i = new _704_BinarySearch();
 //        System.out.println(i.search(new int[]{-1,0,3,5,9,12}, 9));
-        System.out.println(i.search(new int[]{-1,0,3,5,9,12}, 2));
+//        System.out.println(i.search(new int[]{-1,0,3,5,9,12}, 2));
+        System.out.println(i.search(new int[]{4,6,10}, 10));
 //        System.out.println(i.search(new int[]{10, 6, 4}, 10));
     }
 }
