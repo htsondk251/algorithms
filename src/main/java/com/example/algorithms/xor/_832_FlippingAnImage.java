962686cd0ea0e2a7cc240498a1d9d41bb12b20a8
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-26 11:38:10 +0800

    xor: 832 Flip And Invert Image - refactor

--- src/main/java/com/example/algorithms/xor/_832_FlippingAnImage.java
+++ src/main/java/com/example/algorithms/xor/_832_FlippingAnImage.java
@@ -10,13 +10,10 @@ public class _832_FlippingAnImage {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image[0].length;
         for (int i = 0; i < image.length; i++) {
-            int[] clone = new int[n];
-            for (int j = 0; j < n; j++) {
-                clone[j] = image[i][j];
-            }
-
-            for (int j = 0; j < n; j++) {
-                image[i][j] = clone[n-1-j] ^ 1;
+            for (int j = 0; j < (n+1)/2; j++) {
+                int temp = image[i][j] ^ 1;
+                image[i][j] = image[i][n-1-j] ^ 1;
+                image[i][n-1-j] = temp;
             }
         }
 
