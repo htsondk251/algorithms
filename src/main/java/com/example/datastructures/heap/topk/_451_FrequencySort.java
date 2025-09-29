f6520e515433ef3dca3834bfebcc38098ebf5aae
son.hoangthanh <son.hoangthanh.d5@vti.com.vn>
2025-09-29 11:06:28 +0800

    topk: 451 Frequency Sort - sort in Map instead

--- src/main/java/com/example/datastructures/heap/topk/_451_FrequencySort.java
+++ src/main/java/com/example/datastructures/heap/topk/_451_FrequencySort.java
@@ -1,6 +1,6 @@
 package com.example.datastructures.heap.topk;
 
-import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @version 1.0
@@ -10,25 +10,23 @@ import java.util.*;
  */
 public class _451_FrequencySort {
     public String sortCharacterByFrequency(String str) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for (char ch : str.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
-        }
-
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap =
-                new PriorityQueue<>((e1, e2) -> e2.getValue() - e1.getValue());
-
-        freq.entrySet().forEach(maxHeap::add);
-        StringBuilder res = new StringBuilder();
-        while (!maxHeap.isEmpty()) {
-            Map.Entry<Character, Integer> entry = maxHeap.poll();
-            for (int i = 0; i < entry.getValue(); i++) {
-                res.append(entry.getKey());
-            }
-        }
-        return res.toString();
+        return str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+                .map(e -> repeat(e.getKey(), e.getValue()))
+                .collect(Collectors.joining());
     }
 
+    private String repeat(Character key, Long count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(key);
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         _451_FrequencySort i = new _451_FrequencySort();
